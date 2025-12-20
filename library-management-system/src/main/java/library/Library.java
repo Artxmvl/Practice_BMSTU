9820c1a271518d35e407ce0c45fd7b09b9209bba
Artxmvl <talakhovarten2007@gmail.com>
2025-12-20 21:14:05 +0300

    Добавлена структура класса Library

---
+++ library-management-system/src/main/java/library/Library.java
@@ -0,0 +1,26 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) { }
+
+    public Book findBookById(int id) { return null; }
+
+    public List<Book> findBooksByAuthor(String author) { return new ArrayList<>(); }
+
+    public boolean borrowBook(int id) { return false; }
+
+    public boolean returnBook(int id) { return false; }
+
+    public List<Book> getAvailableBooks() { return new ArrayList<>(); }
+
+    public void printOperationLog() { }
+}
